e877a3621c79e4e9efc038f8124638d36e737776
Fahim Sabet <fahim.sabet@icloud.com>
2023-10-08 17:18:20 -0400

    my sunday project

---
+++ src/test/test/login.java
@@ -0,0 +1,10 @@
+package test.test;
+
+public class login {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is a test project to be able to push it to the github");
+    }
+
+}
